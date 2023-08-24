51502594ad4598b4b4be19fe54b66f9aa01bbe37
손경남 <38928298+hand2441@users.noreply.github.com>
2023-08-24 21:15:29 +0900

    클래스형 컴포넌트를 함수형 컴포넌트로 변환

--- react-tictactoe-app/src/components/Board.js
+++ react-tictactoe-app/src/components/Board.js
@@ -1,49 +1,96 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Square from './Square'
 import "./Board.css"
 
-export default class board extends Component {
+// 함수형
+const Board = () => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null)
-        }
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+
+    const status = `Next player ${xIsNext ? 'X' : 'O'}`
+    const handleClick = (i) => {
+        const changeSquares = squares.slice();
+        changeSquares[i] = xIsNext ? 'X' : 'O';
+        setSquares(changeSquares);
+        setXIsNext(current => !xIsNext);
     }
 
-    handleClick(i) {
-        const changeSquares = this.state.squares.slice();
-        changeSquares[i] = 'X';
-        this.setState({ squares: changeSquares});
-    }
-
-    renderSquare(i) {
+    const renderSquare = (i) => {
         return <Square
-                    value={this.state.squares[i]}
-                    onClick={() => this.handleClick(i)}
+                    value={squares[i]}
+                    onClick={() => handleClick(i)}
                 />
     }
 
-    render() {
-        return (
-            <div>
-                <div className='status'>Next Player: X, O</div>
-                <div className='board-row'>
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className='board-row'>
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className='board-row'>
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+    return (
+        <div>
+            <div className='status'>{status}</div>
+            <div className='board-row'>
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
-        )
-    }
+            <div className='board-row'>
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className='board-row'>
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    )
 }
+
+export default Board;
+
+
+// 클래스형
+// export default class board extends Component {
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             squares: Array(9).fill(null)
+//         }
+//     }
+
+//     handleClick(i) {
+//         const changeSquares = this.state.squares.slice();
+//         changeSquares[i] = 'X';
+//         this.setState({ squares: changeSquares});
+//     }
+
+//     renderSquare(i) {
+//         return <Square
+//                     value={this.state.squares[i]}
+//                     onClick={() => this.handleClick(i)}
+//                 />
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <div className='status'>Next Player: X, O</div>
+//                 <div className='board-row'>
+//                     {this.renderSquare(0)}
+//                     {this.renderSquare(1)}
+//                     {this.renderSquare(2)}
+//                 </div>
+//                 <div className='board-row'>
+//                     {this.renderSquare(3)}
+//                     {this.renderSquare(4)}
+//                     {this.renderSquare(5)}
+//                 </div>
+//                 <div className='board-row'>
+//                     {this.renderSquare(6)}
+//                     {this.renderSquare(7)}
+//                     {this.renderSquare(8)}
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
